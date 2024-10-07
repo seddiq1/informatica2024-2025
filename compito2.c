82ca9317409285275c7bf2767ef929e713e5506c
seddiq1 <seddiq.daoudi@galileiostiglia.edu.it>
2024-10-07 11:30:54 +0000

    normali

--- compito2.c
+++ compito2.c
@@ -8,12 +8,12 @@ int main(){
     int C = 0;
     int medio = 0;
 
-    printf("inserisci il primo valore il piu grande %d :", A);
+    printf("inserisci il primo valore il piu grande %d :", A);      // tutto sbagliato 
     scanf("%d", &A);
     printf("inserisci il secondo valore %d :", B);
     scanf("%d", &B);
     printf("inserisci il terzio valore il piu piccolo %d :", C);
-    scanf("%d", &C);
+    scanf("%d", &C);                                                // deve risolverlo con "if"
     medio = (A+B+C)/3 ;
 
     printf("il primo valore è il piu grande %d il secondo valore è il piu piccolo%d il medio è %d:", A, C, medio);
---
+++ informatica/02_20241007.c
@@ -0,0 +1,29 @@
+/*stabilire seun numero è dispari controllando
+l'ultima cifra meno significativa*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    int num , cifra;
+    num=0;
+    cifra=0;
+
+    printf("inserire un numero:");
+    scanf("%d",&num);
+
+    cifra= num%10;
+    if(cifra%2==0)    
+        printf("il numero %d é pari",num);
+    else
+        printf("il numero %d é dispari",num);
+    
+
+
+
+
+    /* code */
+    return 0;
+}
+)
